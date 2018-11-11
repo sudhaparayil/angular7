ba302eb24f055681e6a68672c121734ce93d7662
sudhaparayil <parayilsudha@gmail.com>
2018-11-11 16:29:34 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,89 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { SignupComponent } from './signup/signup.component';
-
+import {CdkTableModule} from '@angular/cdk/table';
 import { UserService } from './shared/user.service';
-import { MatToolbarModule, MatFormFieldModule, MatInputModule, MatOptionModule, MatSelectModule, MatIconModule, MatButtonModule, MatCardModule, MatTableModule, MatDividerModule, MatSnackBarModule } from '@angular/material';
+import { MatToolbarModule,
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+ 
+  MatSortModule,
+  
+  MatTabsModule,
+ 
+  MatTooltipModule,
+  MatStepperModule,
+ 
+  MatFormFieldModule,
+   
+    MatOptionModule, 
+    
+   
+       MatTableModule,
+        MatDividerModule,
+         MatSnackBarModule } from '@angular/material';
 import { SearchComponent } from './search/search.component';
-
+@NgModule({
+  exports: [
+    CdkTableModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+  ],
+  declarations: [],
+ 
+})
+export class DemoMaterialModule {}
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,17 +107,8 @@ import { SearchComponent } from './search/search.component';
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatOptionModule,
-    MatSelectModule,
-    MatIconModule,
-    MatButtonModule,
-    MatCardModule,
-    MatTableModule,
-    MatDividerModule,
-    MatSnackBarModule
+  
+    DemoMaterialModule
   
   ],
   providers: [UserService],
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,24 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewChild} from '@angular/core';
 import { UserService } from '../shared/user.service'
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  user={ _id: String,
+        title: String,
+        
+          }
+  // user:any;
   userId:any;
   result:any;
   allusers:any;
-  constructor(private userService: UserService) { }
+  displayedColumns = [ 'id','name','title','action'];
+  dataSource: MatTableDataSource<any>;
+  notExist =false;
 
+  constructor(private userService: UserService) { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
   ngOnInit() {
     this.userService.getalluser().subscribe(users => {
       this.allusers=users;
     // console.log(users)
+    this.dataSource = new MatTableDataSource(this.allusers);
+    // console.log(this.dataSource);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
 
     });
   }
-  
+  // loadToDataTable(data){
+
+  //   this.notExist =false;
+  //   if(data.length == 0 || data==null){
+  //     this.notExist = true;
+  //   }
+  //   this.dataSource = new MatTableDataSource(data);
+  //   // console.log(this.dataSource);
+  //   this.dataSource.paginator = this.paginator;
+  //   this.dataSource.sort = this.sort;
+  // }
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
   //   console.log(newObj)
   // }
   dataChanged(newObj) {
@@ -32,7 +62,7 @@ export class SearchComponent implements OnInit {
        
        this.userService.getUsersearch(newObj).subscribe(user => {
          console.log(user);
-         this.result = user;
+         this.allusers = user;
        });
      }
  
@@ -50,5 +80,23 @@ export class SearchComponent implements OnInit {
     });
 
 
+  }
+
+  getEditId(id){
+    //  console.log(id);
+    // this.showSpinner =false;
+      this.userService.getUsersearch(id).subscribe(data=>{
+        // console.log( data);
+        
+        this.user = data;
+        console.log(this.user);
+        // this.student.photoSrc = '../assets/photo_upload/' + data.photo;
+        // console.log( this.student);
+    
+      });
+  
+  }
+  updateUser(user){
+    console.log(user)
   }
 }
